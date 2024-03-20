12a9a78bb537ab87e608500aaa9c15fcf8a83738
Daniel-Itzhaki <Daniel.itz16@gmail.com>
2024-03-20 21:49:40 +0200

    check

--- index.js
+++ index.js
@@ -3,52 +3,62 @@ const progress = document.querySelector('.gradient-bar');
 
 fileInput.addEventListener('change', selectFile);
 
-var uploadedFiles = [];
+const MBUnit = 1024 * 1024;
+const maxSize = 10;
+
+let uploadedFiles = [];
 
 function openFileExp() {
     fileInput.click();
 }
 
 function selectFile() {
-    const filePattern1 = /jpg$/;
-    const filePattern2 = /jpeg$/;
-    const filePattern3 = /png$/;
-    const filePattern4 = /gif$/;
+    const filePattern = /\.(jpg|jpeg|png|gif)$/;
 
-    var files = fileInput.files;
+    let files = fileInput.files;
     for (let i = 0; i < files.length; i++) {
-        if (!filePattern1.test(files[i].type) && !filePattern2.test(files[i].type) &&
-            !filePattern3.test(files[i].type) && !filePattern4.test(files[i].type)) {
+        if (filePattern.test(files[i].name)) {
+            if (fileInput.files[0].size > maxSize * MBUnit) {
+                alert("File size is to BIG. 10MB or lower!")
+                return;
+            }
+            else
+                uploadedFiles.push(files[i]);
+        }
+
+        else {
             alert("Please select valid image files.");
             return;
         }
-
-        else if (fileInput.files[0].size > 10 * 1024 * 1024) {
-            alert("File size is to BIG. 10MB or lower!")
-            return;
-        }
-
-        uploadedFiles.push(files[i]);
     }
 
+    displayFiles(uploadedFiles);
+    checkStorageSize(files);
+}
+
+function checkStorageSize() {
     let totalSize = 0;
     for (let i = 0; i < uploadedFiles.length; i++) {
         totalSize += uploadedFiles[i].size;
-        if (totalSize > 10 * (1024 * 1024)) {
+        if (totalSize > maxSize * MBUnit) {
             alert("no more space");
             return;
         }
     }
+    const MBRemaining = (totalSize / MBUnit).toFixed(2);
+    document.getElementById('spaceUsed').textContent = MBRemaining + ' MB';
 
-    const MBSize = (totalSize / (1024 * 1024)).toFixed(2);
-    document.getElementById('spaceUsed').textContent = MBSize + ' MB';
-
-    const spaceLeft = (10 - MBSize).toFixed(2);
+    const spaceLeft = (maxSize - MBRemaining).toFixed(2);
     document.getElementById('spaceLeft').textContent = spaceLeft + ' MB left';
 
-    let gradientBarPercent = (MBSize / 10) * 100;
-    progress.style.width = `${gradientBarPercent}%`
-    displayFiles(uploadedFiles);
+    UpdateGradientBar(MBRemaining);
+
+}
+
+function UpdateGradientBar(MBRemaining) {
+    let gradientBarPercent = (MBRemaining / maxSize) * 100;
+    progress.style.width = `${gradientBarPercent}%`;
+
 }
 
 function displayFiles(files) {
@@ -57,8 +67,7 @@ function displayFiles(files) {
     fileList.innerHTML = '';
     files.forEach(file => {
         const listItem = document.createElement('li');
-        listItem.textContent = file.name + ' - ' + (file.size / (1024 * 1024)).toFixed(2) + ' MB';
+        listItem.textContent = file.name + ' - ' + (file.size / MBUnit).toFixed(2) + ' MB';
         fileList.appendChild(listItem);
     });
 }
-
